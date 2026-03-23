f06f9c036a556b21969ad7eb04d1e46b394cd626
FernandoChu <17756312+FernandoChu@users.noreply.github.com>
2026-03-23 18:38:15 +0100

    remove translations

--- hw_3.v
+++ hw_3.v
@@ -2,6 +2,7 @@ Unset Universe Checking.
 Require Export UniMath.Foundations.All.
 
 (* For the following exercises you can use the following results from previous exercise sessions without proofs. *)
+(* You can also use results from the previous homework. *)
 
 Theorem hlevel_cumulative  {n : nat} {T : UU} (h : isofhlevel n T) : isofhlevel (S n) T.
 Proof.
@@ -19,15 +20,11 @@ Proof.
 
 (* Exercise 1 *)
 
-(* In the last homework you showed the two definitions of isaprop were *logically equivalent*. Now show that the definitions are *equivalent*. *)
-
 Theorem prop_thm {P : UU} : (isaprop P) ≃ (∏ x y : P, x = y).
 Proof.
   Admitted.
 
 (* Exercise 2 *)
-
-(* Show that the dependent product type former commutes with `isaprop`.*)
 
 Theorem prop_commutes_Π {A : UU} {B : A → UU} (p : ∏ x : A, isaprop (B x)) : isaprop (∏ x : A, (B x)).
 Proof.
@@ -35,17 +32,11 @@ Proof.
 
 (* Exercise 3 *)
 
-(* Show that isweq f (is-contr f in Rijke) is a proposition. *)
-
 Theorem isweq_is_prop {A B : UU} (f : A → B) : isaprop (isweq f).
 Proof.
   Admitted.
 
 (* Exercise 4 *)
-
-(* An equivalence between propositions is (equivalent to) a logical equivalence. *)
-
-(* In the last homework you showed that an equivalence between propositions is *logically equivalent* to a logical equivalence. Now show that an equivalence between propositions is *equivalent* to a logical equivalence.*)
 
 Theorem equiv_of_prop {P Q : hProp} : (P ≃ Q) ≃ (P <-> Q).
 Proof.
